c5f17b06bed3466626e9a74b954bf3b43dfd2478
niraj gupta <actuallynirajgupta@gmail.com>
2025-07-04 08:23:28 +0530

    implement account creation and management features with DTOs and exception handling

---
+++ src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class AccountsDto {
+
+  private Long accountNumber;
+
+  private String accountType;
+
+  private String branchAddress;
+}
